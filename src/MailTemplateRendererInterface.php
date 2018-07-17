2b3db11757a9dce6717f7fb24918455261ee15a4
George Steel <george@net-glue.co.uk>
2018-07-17 18:06:50 +0100

    Add interface for the template renderer interface to target as an alias in config

---
+++ src/MailTemplateRendererInterface.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace NetglueMail;
+
+use Zend\Expressive\Template\TemplateRendererInterface;
+
+interface MailTemplateRendererInterface extends TemplateRendererInterface
+{
+}
