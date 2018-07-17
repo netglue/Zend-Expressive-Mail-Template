ae2801a294f98fe33c0df6fcc0d6baa6a022bc2b
George Steel <george@net-glue.co.uk>
2018-07-17 17:54:53 +0100

    Tidy up exceptions. Exception interface should implement throwable

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -1,5 +1,8 @@
 <?php
+declare(strict_types=1);
 
 namespace NetglueMail\Exception;
 
-class RuntimeException extends \RuntimeException implements ExceptionInterface {}
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+}
