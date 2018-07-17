7284c0e6b138f4b4fa36f8351058fb718785b38c
George Steel <george@net-glue.co.uk>
2018-07-17 17:56:54 +0100

    Tidy up test framework and add phpcs config

--- test/NetglueMailTest/TestCase.php
+++ test/NetglueMailTest/TestCase.php
@@ -1,47 +1,8 @@
 <?php
+declare(strict_types=1);
 
 namespace NetglueMailTest;
 
-use NetglueMail\ConfigProvider;
-use Zend\ServiceManager\Config;
-use Zend\ServiceManager\ServiceManager;
-use Zend\Expressive\ConfigManager\ConfigManager;
-use Zend\Expressive\ConfigManager\PhpFileProvider;
-
-
-class TestCase extends \PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
-
-    /**
-     * @var ContainerInterface
-     */
-    protected static $container;
-
-
-    public static function setUpBeforeClass()
-    {
-        $configManager = new ConfigManager([
-            ConfigProvider::class,
-            new PhpFileProvider(__DIR__ . '/../config/{{,*.}global,{,*.}local}.php'),
-        ]);
-        $config = $configManager->getMergedConfig();
-        $config['debug']                = true;
-        $config['config_cache_enabled'] = false;
-
-        // Build container
-        $container = new ServiceManager();
-        (new Config($config['dependencies']))->configureServiceManager($container);
-
-        // Inject config
-        $container->setService('config', $config);
-
-        self::$container = $container;
-    }
-
-    public static function tearDownAfterClass()
-    {
-        // Clean up
-        self::$container = null;
-    }
-
 }
